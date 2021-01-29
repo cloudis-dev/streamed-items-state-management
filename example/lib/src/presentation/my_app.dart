137961e4c64031ec8eccd97a25d85c8bb7ad5423
Kristián Balaj <kiko.balaj@gmail.com>
2021-01-29 20:59:03 +0100

    fix(*): uncommented commented out code

--- example/lib/src/presentation/my_app.dart
+++ example/lib/src/presentation/my_app.dart
@@ -1,7 +1,9 @@
+import 'package:example/src/data/product_model.dart';
 import 'package:example/src/presentation/providers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/all.dart';
+import 'package:streamed_items_state_management/streamed_items_state_management.dart';
 
 /// Scroll list view with products.
 /// The items are updated.
@@ -15,14 +17,14 @@ class MyApp extends HookWidget {
       home: Scaffold(
         body: CustomScrollView(
           slivers: [
-            // SliverPagedList<ProductModel>(
-            //   itemsState: itemsState,
-            //   itemBuilder: (context, item) => ListTile(
-            //     title: Text(item.uniqueId),
-            //     subtitle: Text('Price: ${item.price}'),
-            //   ),
-            //   requestData: context.read(allProductsStateProvider).requestData,
-            // )
+            SliverPagedList<ProductModel>(
+              itemsState: itemsState,
+              itemBuilder: (context, item) => ListTile(
+                title: Text(item.uniqueId),
+                subtitle: Text('Price: ${item.price}'),
+              ),
+              requestData: context.read(allProductsStateProvider).requestData,
+            )
           ],
         ),
       ),
