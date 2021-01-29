7a463b35128d640c7ac3dff1906e3de77e8a702f
Kristián Balaj <kiko.balaj@gmail.com>
2021-01-29 20:48:18 +0100

    feat(*): using now the github package

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,3 +1,7 @@
-// void main() {
-//   runApp(ProviderScope(child: MyApp()));
-// }
+import 'package:example/src/presentation/my_app.dart';
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/all.dart';
+
+void main() {
+  runApp(ProviderScope(child: MyApp()));
+}
--- example/lib/src/presentation/products_state_notifier.dart
+++ example/lib/src/presentation/products_state_notifier.dart
@@ -1,23 +1,26 @@
+import 'package:example/src/data/product_model.dart';
+import 'package:streamed_items_state_management/streamed_items_state_management.dart';
+
 /// State management for streamed [ProductModel]s.
 ///
 /// This is the version where all the products are received by a single stream
 /// with all the updates (i.e the products are not paginated).
-// class ProductsStateNotifier
-//     extends SingleStreamItemsStateNotifier<ProductModel> {
-//   ProductsStateNotifier(
-//     Stream<ItemsStateStreamBatch> Function() createStream,
-//   ) : super(
-//           createStream,
-//           ProductsItemsHandler(),
-//         );
-// }
-//
-// /// The items are ordered based on the price ascending.
-// /// The unique representation of the item is the [ProductModel.uniqueId].
-// class ProductsItemsHandler extends ItemsHandler<ProductModel, String> {
-//   ProductsItemsHandler()
-//       : super(
-//           (a, b) => a.price.compareTo(b.price),
-//           (a) => a.uniqueId,
-//         );
-// }
+class ProductsStateNotifier
+    extends SingleStreamItemsStateNotifier<ProductModel> {
+  ProductsStateNotifier(
+    Stream<ItemsStateStreamBatch> Function() createStream,
+  ) : super(
+          createStream,
+          ProductsItemsHandler(),
+        );
+}
+
+/// The items are ordered based on the price ascending.
+/// The unique representation of the item is the [ProductModel.uniqueId].
+class ProductsItemsHandler extends ItemsHandler<ProductModel, String> {
+  ProductsItemsHandler()
+      : super(
+          (a, b) => a.price.compareTo(b.price),
+          (a) => a.uniqueId,
+        );
+}
