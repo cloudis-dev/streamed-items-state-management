0180dd08fc50dd5551523f0c739641a5db0211e2
Kristián Balaj <kiko.balaj@gmail.com>
2021-01-29 19:06:32 +0100

    feat(*): added example

---
+++ example/src/data/mocked_db_service.dart
@@ -0,0 +1,53 @@
+import 'dart:async';
+import 'dart:math';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:streamed_items_state_management/streamed_items_state_management.dart';
+import 'package:tuple/tuple.dart';
+
+import 'product_model.dart';
+
+class MockedDbService {
+  /// Get all the products stream.
+  /// There are 2 mocked products (`A`, `B`).
+  /// Every 3 seconds an update of the price of the `A` product is created.
+  /// The price is set randomly from 0 to 100.
+  Stream<ItemsStateStreamBatch<ProductModel>> getAllProductsStream() {
+    final mockedData = [ProductModel('A', 5), ProductModel('B', 6)];
+
+    final streamCtrl = StreamController<ItemsStateStreamBatch<ProductModel>>();
+
+    streamCtrl.onListen = () {
+      // Adding the initial batch
+      streamCtrl.add(
+        ItemsStateStreamBatch(
+          mockedData.map((e) => Tuple2(DocumentChangeType.added, e)).toList(),
+        ),
+      );
+    };
+
+    // Update the 'A' product's price every 3 seconds to a random number from 0 to 100
+    final random = Random();
+    final sub = Stream.periodic(Duration(seconds: 3)).listen(
+      (event) {
+        streamCtrl.add(
+          ItemsStateStreamBatch(
+            [
+              Tuple2(
+                DocumentChangeType.modified,
+                ProductModel('A', random.nextDouble() * 100),
+              )
+            ],
+          ),
+        );
+      },
+    );
+
+    streamCtrl.onCancel = () async {
+      await streamCtrl.close();
+      await sub.cancel();
+    };
+
+    return streamCtrl.stream;
+  }
+}
---
+++ example/src/presentation/providers/providers.dart
@@ -0,0 +1,15 @@
+import 'package:hooks_riverpod/all.dart';
+
+import '../../data/mocked_db_service.dart';
+import '../view_models/products_state_notifier.dart';
+
+final mockedDbServiceProvider = Provider((_) => MockedDbService());
+
+final allProductsStateProvider =
+    ChangeNotifierProvider.autoDispose<ProductsStateNotifier>(
+  (ref) {
+    final dbService = ref.watch(mockedDbServiceProvider);
+
+    return ProductsStateNotifier(dbService.getAllProductsStream)..requestData();
+  },
+);
