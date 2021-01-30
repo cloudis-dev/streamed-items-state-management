0965768514ec17954cd6b26cef24f5c41934e174
Kristián Balaj <kiko.balaj@gmail.com>
2021-01-30 10:59:04 +0100

    style(*): fixing lint

--- lib/streamed_items_state_management.dart
+++ lib/streamed_items_state_management.dart
@@ -3,8 +3,8 @@ library streamed_items_state_management;
 // Data
 export 'src/data/change_status.dart';
 export 'src/data/items_handler.dart';
-export 'src/data/items_state_stream_batch.dart';
 export 'src/data/items_state.dart';
+export 'src/data/items_state_stream_batch.dart';
 // Slivers
 export 'src/presentation/slivers/implementations/sliver_paged_grid.dart';
 export 'src/presentation/slivers/implementations/sliver_paged_list.dart';
